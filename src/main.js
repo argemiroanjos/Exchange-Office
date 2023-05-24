d884c736dac6f99f487f72afebc614412c308eb5
Argemiro dos Anjos <argemirodosanjos17@gmail.com>
2023-05-24 00:01:44 -0300

    Adiciona funções de criação e renderização

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './style.css'
 import './reset.style.css'
+
+const ulElement = document.querySelector('.container main ul')
+
+// Função que retorna o <b> e a <li> preenchidos.
+const createLiElement = (name, value) => {
+    const liElement = document.createElement('li');
+    liElement.innerHTML = `
+    <b>${name}</b>
+    <span>${value}</span>
+    `
+    return liElement;
+};
+
+const renderCoins = (coins, baseCoin) => {
+    ulElement.innerHTML = '';
+    coins.forEach(coin => {
+        const name = coin.name;
+        const value = coin.value;
+
+        const liElement = createLiElement(name, value);
+        ulElement.appendChild(liElement)
+    });
+};
